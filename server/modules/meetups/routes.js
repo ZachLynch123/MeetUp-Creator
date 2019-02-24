49d1ec41cbb8a2dde46284990c198d4d1f2e27e1
HankBedford <Zachary.Lynch1337@gmail.com>
2019-02-24 10:07:29 -0800

    Finished controller and created a post route

---
+++ server/modules/meetups/routes.js
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import * as MeetupController from './controller';
+
+const routes = new Router();
+
+// post new meetup
+
+routes.post('/meetups', MeetupController.createMeetup);
